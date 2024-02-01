2e453f2148f006e59471818db322301911db626e
Vardhan077 <vardhanmarriwada777@gmail.com>
2024-02-01 21:14:10 +0530

    another 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './styles/App.css';
 import Home from './components/home';
 import Products from './components/Products';
-import CheckOut from './components/checkout';
+
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -11,7 +11,6 @@ const App = () =>(
         <Routes>
           <Route exact path='/' Component={Home}></Route>
           <Route exact path='/products' Component={Products}></Route>
-          <Route exact path='/checkout' Component={CheckOut}></Route>
         </Routes>
       </BrowserRouter>
 )
